e811169bbf59e50468eb5567c10f1391d1d4869c
Samuel Gerga <samuelgerga@gmail.com>
2026-04-26 19:30:56 -0300

    Commit2: Adicionado CRUD Editora

---
+++ src/main/java/br/ufscar/dc/dsw/controller/EditoraController.java
@@ -0,0 +1,125 @@
+package br.ufscar.dc.dsw.controller;
+
+import br.ufscar.dc.dsw.dao.EditoraDAO;
+import br.ufscar.dc.dsw.domain.Editora;
+
+import java.io.IOException;
+import java.util.List;
+import java.sql.SQLException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns = "/editoras/*")
+public class EditoraController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private EditoraDAO dao;
+
+    @Override
+    public void init() {
+        dao = new EditoraDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        String action = request.getPathInfo();
+        if (action == null) {
+            action = "";
+        }
+
+        try {
+            switch (action) {
+                case "/cadastro":
+                    apresentaFormCadastro(request, response);
+                    break;
+                case "/insercao":
+                    insere(request, response);
+                    break;
+                case "/remocao":
+                    remove(request, response);
+                    break;
+                case "/edicao":
+                    apresentaFormEdicao(request, response);
+                    break;
+                case "/atualizacao":
+                    atualize(request, response);
+                    break;
+                default:
+                    lista(request, response);
+                    break;
+            }
+        } catch (RuntimeException | IOException | ServletException e) {
+            throw new ServletException(e);
+        }
+    }
+
+
+    private void lista(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Editora> listaEditoras = dao.getAll();
+        request.setAttribute("listaEditoras", listaEditoras); //
+        request.setAttribute("contextPath", request.getContextPath().replace("/", ""));
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/editora/lista.jsp"); //
+        dispatcher.forward(request, response);
+    }
+
+    private void apresentaFormCadastro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/editora/formulario.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void apresentaFormEdicao(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long id = Long.parseLong(request.getParameter("id"));
+        Editora editora = dao.get(id);
+        request.setAttribute("editora", editora); // Manda a editora para o formulário
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/editora/formulario.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void insere(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String cnpj = request.getParameter("cnpj");
+        String nome = request.getParameter("nome");
+
+        Editora editora = new Editora(cnpj, nome);
+        dao.insert(editora);
+        response.sendRedirect("lista");
+    }
+
+    private void atualize(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        Long id = Long.parseLong(request.getParameter("id"));
+        String cnpj = request.getParameter("cnpj");
+        String nome = request.getParameter("nome");
+
+        Editora editora = new Editora(id, cnpj, nome);
+        dao.update(editora);
+        response.sendRedirect("lista");
+    }
+    // Nessa parte, eu achei útil lançar um aviso quando a editoras possuir livros apontados para ela. Antes aparecia apenas erro.
+    private void remove(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long id = Long.parseLong(request.getParameter("id"));
+        Editora editora = new Editora(id);
+        try {
+            dao.delete(editora);
+            response.sendRedirect("lista");
+        } catch (SQLException e) {
+
+            if (e.getErrorCode() == 1451) {
+                request.setAttribute("mensagens", "Esta editora possui livros cadastrados e não pode ser excluída!");
+                lista(request, response);
+            } else {
+                throw new ServletException(e);
+            }
+        }
+    }
+}
